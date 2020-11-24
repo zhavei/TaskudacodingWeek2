c0d18c7168ad7cad6991846586fe03815e4499ce
zhavei <64249579+zhavei@users.noreply.github.com>
2020-11-24 13:31:32 +0700

    update

--- app/src/main/java/com/zhavei/taskudacodingweek2/bmicalculator/BmiMainActivity.kt
+++ app/src/main/java/com/zhavei/taskudacodingweek2/bmicalculator/BmiMainActivity.kt
@@ -4,9 +4,11 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.zhavei.taskudacodingweek2.R
-import kotlinx.android.synthetic.main.bmi_activity_main.*
 
 
 class BmimainActivity : AppCompatActivity() {
@@ -15,18 +17,62 @@ class BmimainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.bmi_activity_main)
 
-
         supportActionBar!!.hide()
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
         window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
 
-        bmi_btn_hitung.setOnClickListener {
-            val height = bmi_et_height.text.toString().toFloat() / 100  // dapet nilai tinggi dan convert ke Meter
-            val weight = bmi_et_weight.text.toString().toFloat()         // dapet nilai berat dari editext kemudian konvert ke float
-            val result : Float = weight/(height*height)  // formula hitung berat badan ideal
-            bmi_tv_result.text = "%.2f".format(result) // format hasil hanya menampilkan 2 digit decimal
-
+        val editextBerat: EditText = findViewById<View>(R.id.bmi_et_berat) as EditText
+        val editextTinggi: EditText = findViewById<View>(R.id.bmi_et_tinggi) as EditText
+        val tvdetailBerat: TextView = findViewById<View>(R.id.bmi_tv_result_info) as TextView
+        val tvHasilCalculate: TextView = findViewById<View>(R.id.bmi_tv_yourindex) as TextView
+        val btnCalculate: Button = findViewById<View>(R.id.bmi_bt_calculate) as Button
+        val btnReset: Button = findViewById<View>(R.id.bmi_bt_reset) as Button
+        btnCalculate.setOnClickListener(View.OnClickListener {
+            val berat = editextBerat.text.toString()
+            val tinggi = editextTinggi.text.toString()
+            if (berat == "") {
+                editextBerat.error = "Berat Badan Anda "
+                editextBerat.requestFocus()
+                return@OnClickListener
+            }
+            if (tinggi == "") {
+                editextTinggi.error = "Tinggi Badan Anda"
+                editextTinggi.requestFocus()
+                return@OnClickListener
+            }
+            val beratToFloat = berat.toFloat()
+            val tinggiTofloat = tinggi.toFloat() / 100
+            val bmiValue = BMICalculate(beratToFloat, tinggiTofloat)
+            tvdetailBerat.text = interpreteBMI(bmiValue)
+            tvHasilCalculate.text = "BMI= $bmiValue"
+        })
+        btnReset.setOnClickListener {
+            editextTinggi.setText("")
+            editextBerat.setText("")
+            tvdetailBerat.text = ""
+            tvHasilCalculate.text = ""
         }
+    }
 
+    fun BMICalculate(weight: Float, height: Float): Float {
+        return weight / (height * height)
+    }
+
+    fun interpreteBMI(bmiValue: Float): String {
+        return when {
+            bmiValue < 16 -> {
+                "Kamu Kurang Berat Badan Bgt!"
+            }
+            bmiValue < 18.5 -> {
+                "Berat Badan Kamu Kurang"
+            }
+            bmiValue < 25 -> {
+                "Normal, Cakep"
+            }
+            bmiValue < 30 -> {
+                "Kamu Kelebihan Berat Badan"
+            }
+            else -> "Kamu Obsesi Gendut apa Obesitas"
+        }
     }
 }
